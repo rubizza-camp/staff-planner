f44ee24aa0c8973fa7b27e8863bca3dc492cb3e7
Artur <rutraniloms@icloud.com>
2019-08-04 18:42:55 +0300

    delete email from model, prepare  for Devise install

--- db/migrate/20190804102940_create_accounts.rb
+++ db/migrate/20190804102940_create_accounts.rb
@@ -3,7 +3,6 @@ class CreateAccounts < ActiveRecord::Migration[5.2]
     create_table :accounts do |t|
       t.string :name, null: false
       t.string :surname, null: false
-      t.string :email, null: false, index: { unique: true }
       t.date :date_of_birth
 
       t.timestamps
