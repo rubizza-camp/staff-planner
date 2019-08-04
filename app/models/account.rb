de838fd8237e9ed04929810eb62831c5b1cf7609
Artur <rutraniloms@icloud.com>
2019-08-04 18:42:02 +0300

    add Devise auth

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,9 @@
 class Account < ApplicationRecord
   validates :name, presence: true
   validates :surname, presence: true
+  validates :email, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
