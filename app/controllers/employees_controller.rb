e1e46b82614612a2410fd3fd971be3121728a149
Artur <rutraniloms@icloud.com>
2019-08-06 16:40:08 +0300

    edit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,64 +1,42 @@
 # frozen_string_literal: true
 
 class EmployeesController < ApplicationController
-  before_action :set_employee, only: %i[show edit update destroy]
+  before_action :set_employee, :set_company, only: %i[show edit update destroy]
 
-  # GET /employees
-  # GET /employees.json
   def index
+    @company = Company.find(params[:company_id])
     @employees = Employee.all
   end
 
-  # GET /employees/1
-  # GET /employees/1.json
   def show; end
 
-  # GET /employees/new
   def new
-    @employee = Employee.new
+    @employee = @company.employee.new
   end
 
-  # GET /employees/1/edit
   def edit; end
 
-  # POST /employees
-  # POST /employees.json
   def create
     @employee = Employee.new(employee_params)
-
-    respond_to do |format|
       if @employee.save
-        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
-        format.json { render :show, status: :created, location: @employee }
       else
-        format.html { render :new }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
+        render :new 
       end
-    end
   end
 
-  # PATCH/PUT /employees/1
-  # PATCH/PUT /employees/1.json
   def update
-    respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
-        format.json { render :show, status: :ok, location: @employee }
+        redirect_to @employee, notice: 'Employee was successfully updated.' 
       else
-        format.html { render :edit }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
+        render :edit 
       end
-    end
   end
 
   # DELETE /employees/1
   # DELETE /employees/1.json
   def destroy
     @employee.destroy
-    respond_to do |format|
-      format.html { redirect_to employees_url, notice: 'Employee was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to employees_url, notice: 'Employee was successfully destroyed.' 
   end
 
   private
@@ -66,6 +44,10 @@ class EmployeesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_employee
     @employee = Employee.find(params[:id])
+  end
+
+  def set_company
+    @company = Company.find(params[:company_id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :companies
   devise_for :accounts
   devise_scope :account do
     root to: "devise/sessions#new"
   end
 
   resources :accounts, except: [:new, :create]
+  resources :companies do
+  	resources :employees
+  end
 end
