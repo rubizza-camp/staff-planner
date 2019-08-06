1b22517436f4bb561818d088acd8a94ae2790dfd
EvgeniyR <richkovevgeniy@mail.ru>
2019-08-06 23:07:31 +0300

    Repair aLowance_days

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+PERIOD_LIST = %w[year month week].freeze
+
 class Rule < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :company_id }
-  validates :period, presence: true, inclusion: { in: %w[year month week] }
+  validates :period, presence: true, inclusion: { in: PERIOD_LIST }
   validates :alowance_days, numericality: { only_integer: true,
                                             greater_than_or_equal_to: 0 }
 
