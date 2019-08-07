96aa4624a196c5e676ca7c2abd6c9c07f1e76995
Artur <rutraniloms@icloud.com>
2019-08-07 14:19:01 +0300

    fix

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -5,7 +5,9 @@ require 'rails_helper'
 RSpec.describe 'Employees', type: :request do
   describe 'GET /employees' do
     it 'works! (now write some real specs)' do
-      get company_employees_path
+      company = Company.new(name: 'Company')
+      company.save
+      get company_employees_path(company_id: company.id)
       expect(response).to have_http_status(200)
     end
   end
