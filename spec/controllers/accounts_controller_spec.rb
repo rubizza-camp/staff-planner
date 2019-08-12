c3516c4355cccec9d87fd331e78956a9ba517a28
Artur <rutraniloms@icloud.com>
2019-08-12 19:37:29 +0300

    edit account_controller spec

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -21,8 +21,8 @@ RSpec.describe AccountsController, type: :controller do
 
   describe 'GET #index' do
     it 'returns a success response' do
-      Account.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      account = Account.create! valid_attributes
+      get :index, params: {id: account.id}, session: valid_session
       expect(response).to be_successful
     end
   end
