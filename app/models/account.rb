0d5649bd442a9d5f66a99f5af34f669c3d664266
Artur <rutraniloms@icloud.com>
2019-08-13 14:34:40 +0300

    sign_in fix

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,5 +10,5 @@ class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => [:email, :password]
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:email]
 end
