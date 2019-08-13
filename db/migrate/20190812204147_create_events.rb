bbf76648174cee9c4574250ea30cd26a64c9fb62
Artur <rutraniloms@icloud.com>
2019-08-13 11:13:34 +0300

    edit spec

--- db/migrate/20190812204147_create_events.rb
+++ db/migrate/20190812204147_create_events.rb
@@ -4,9 +4,9 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.datetime :start_period, null: false
       t.datetime :end_period, null: false
       t.string :reason
-      t.references :employee, foreign_key: true, null: false
-      t.references :company, foreign_key: true
-      t.references :rule, foreign_key: true, null: false
+      t.references :employee, foreign_key: true, null: false, index: true
+      t.references :company, foreign_key: true, index: true
+      t.references :rule, foreign_key: true, null: false, index: true
 
       t.timestamps
     end
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     start_period { "2019-08-12 23:41:47" }
     end_period { "2019-08-12 23:41:47" }
     reason { "MyString" }
-    employee { nil }
-    company { nil }
-    rule { nil }
+    employee
+  	company
+  	rule
   end
 end
