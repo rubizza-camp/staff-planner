b3989e1a9dc0d7bb0c8ce3de9c5902148038d33d
Artur <rutraniloms@icloud.com>
2019-08-13 19:28:30 +0300

    before do spec fix

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -3,102 +3,75 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController do
-  render_views
+  before(:each) do
+    @account = FactoryBot.create(:account) 
+    @company = FactoryBot.create(:company)
+    sign_in @account
+  end
 
   describe 'GET index' do
-    let!(:company) { create(:company) }
-    let(:account) { create(:account) }
-
     it 'has a 200 status code' do
-      sign_in account
       get :index
       expect(response.status).to eq(200)
     end
   end
 
   describe 'GET show' do
-    let(:company) { create(:company) }
-    let(:account) { create(:account) }
-
     it 'has a 200 status code' do
-      sign_in account
-      get :show, params: { id: company.id }
+      get :show, params: { id: @company.id }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'GET new' do
-    let(:account) { create(:account) }
-    
     it 'has a 200 status code' do
-      sign_in account
       get :new
       expect(response.status).to eq(200)
     end
   end
 
   describe 'GET calendar' do
-    let(:company) { create(:company) }
-    let(:account) { create(:account) }
-
     it 'has a 200 status code' do
-      sign_in account
-      get :calendar, params: { company_id: company.id }
+      get :calendar, params: { company_id: @company.id }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'GET edit' do
-    let(:company) { create(:company) }
-    let(:account) { create(:account) }
-
     it 'has a 200 status code' do
-      sign_in account
-      get :edit, params: { id: company.id }
+      get :edit, params: { id: @company.id }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'POST create' do
-    let(:account) { create(:account) }
     it 'creates company' do
-      sign_in account
       post :create, params: { company: { name: 'Any Name' } }
       expect(response).to redirect_to(Company.last)
     end
 
     it 'can not creates company' do
-      sign_in account
       post :create, params: { company: { name: nil } }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'PUT update' do
-    let(:company) { create(:company) }
-    let(:account) { create(:account) }
-
     it 'updates company' do
-      sign_in account
-      put :update, params: { company: { name: 'Any Name' }, id: company.id }
-      expect(company.reload.name).to eq('Any Name')
+      put :update, params: { company: { name: 'Any Name' }, id: @company.id }
+      expect(@company.reload.name).to eq('Any Name')
       expect(response).to redirect_to(Company.last)
     end
 
     it 'can not updates company' do
-      sign_in account
-      put :update, params: { company: { name: nil }, id: company.id }
+      put :update, params: { company: { name: nil }, id: @company.id }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'DELETE destroy' do
-    let(:company) { create(:company) }
-    let(:account) { create(:account) }
-
     it 'deletes company' do
-      sign_in account
-      delete :destroy, params: { id: company.id }
+      delete :destroy, params: { id: @company.id }
       expect(response).to redirect_to(companies_url)
       expect(Company.count).to eq(0)
     end
--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -5,55 +5,47 @@ require 'rails_helper'
 RSpec.describe EmployeesController do
   render_views
 
-  describe 'GET show' do
-    let(:employee) { create(:employee) }
-     let(:account) { create(:account, email: '222@mail.ru') }
+  before(:each) do
+    @account = FactoryBot.create(:account, email: '222@mail.ru') 
+    @employee = FactoryBot.create(:employee)
+    sign_in @account
+  end
 
+  describe 'GET show' do
     it 'has a 200 status code' do
-      sign_in account
-      get :show, params: { company_id: employee.company_id, id: employee.id }
+      get :show, params: { company_id: @employee.company_id, id: @employee.id }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'GET new' do
-    let(:employee) { create(:employee) }
-     let(:account) { create(:account, email: '222@mail.ru') }
     it 'has a 200 status code' do
-      sign_in account
-      get :new, params: { company_id: employee.company_id }
+      get :new, params: { company_id: @employee.company_id }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'GET edit' do
-    let(:employee) { create(:employee) }
-     let(:account) { create(:account, email: '222@mail.ru') }
-
     it 'has a 200 status code' do
-      sign_in account
-      get :edit, params: { company_id: employee.company_id, id: employee.id }
+      get :edit, params: { company_id: @employee.company_id, id: @employee.id }
       expect(response.status).to eq(200)
     end
   end
 
   describe 'POST create' do
-    let(:company) { create(:company) }
-    let(:account) { create(:account) }
-
     it 'creates employeer' do
-      sign_in account
+      company = FactoryBot.create(:company)
       post :create, params: { is_enabled: true,
                               position: 'Boss',
                               start_day: '2019-02-02',
                               company_id: company.id,
-                              account_id: account.id }
+                              account_id: @account.id }
       expect(response.status).to eq(200)
     end
 
     it 'can not creates employeer' do
-      account2 = FactoryBot.build(:account, email: '222@mail.ru')
-      sign_in account
+      company = FactoryBot.create(:company)
+      account2 = FactoryBot.build(:account, email: '111@mail.ru')
       post :create, params: { is_enabled: true,
                               position: nil,
                               start_day: '2019-02-02',
@@ -64,30 +56,21 @@ RSpec.describe EmployeesController do
   end
 
   describe 'PUT update' do
-    let(:employee) { create(:employee)}
-    let(:account) { create(:account, email: '222@mail.ru') }
-
     it 'updates employee' do
-      sign_in account
-      put :update, params: {  company_id: employee.company_id, id: employee.id, start_day: '2019-02-02', position: 'boss' }
+      put :update, params: {  company_id: @employee.company_id, id: @employee.id, start_day: '2019-02-02', position: 'boss' }
       expect(response.status).to eq(302)
     end
 
     it 'can not updates employee' do
-      sign_in account
-      put :update, params: { company_id: employee.company_id, id: employee.id, is_enabled: nil }
+      put :update, params: { company_id: @employee.company_id, id: @employee.id, is_enabled: nil }
       expect(response.status).to eq(302)
     end
   end
 
   describe 'DELETE destroy' do
-    let(:employee) { create(:employee) }
-    let(:account) { create(:account, email: '222@mail.ru') }
-
     it 'deletes employee' do
-      sign_in account
-      delete :destroy, params: { company_id: employee.company_id, id: employee.id }
-      expect(response).to redirect_to(company_path(employee.company_id))
+      delete :destroy, params: { company_id: @employee.company_id, id: @employee.id }
+      expect(response).to redirect_to(company_path(@employee.company_id))
       expect(Employee.count).to eq(0)
     end
   end
