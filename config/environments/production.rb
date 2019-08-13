857fe1d92727dee48acb4b7bd875b1a6ae14df37
Artur <rutraniloms@icloud.com>
2019-08-13 18:08:05 +0300

    hash new style

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => "localhost" }
+   config.action_mailer.default_url_options = { host: :localhost }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
