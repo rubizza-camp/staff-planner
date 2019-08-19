5adda41263644897134fc63681354002462f302b
EvgeniyR <richkovevgeniy@mail.ru>
2019-08-19 14:46:09 +0300

    repair test

--- spec/operations/companies_create_spec.rb
+++ spec/operations/companies_create_spec.rb
@@ -3,26 +3,33 @@ require 'rails_helper'
 RSpec.describe Companies::Create do
   let(:account) { create(:account) }
 
-  describe 'Create company and employee' do
-    it 'creates company and employee' do
-      company_params = { name: 'Any Name' }
-      Companies::Create.new.call(company_params, account.id)
-      expect(Company.count).to eq(1)
-      expect(Employee.count).to eq(1)
+  describe '#call' do
+    context 'with valid params' do
+      let(:company_params) { { name: 'Any Name' } }
+      subject(:call) { Companies::Create.new.call(company_params, account.id) }
+
+      it 'creates company and employee' do
+        expect { call }.to change { Employee.count }.by(1)
+         .and change {Company.count}.by(1)
+      end
     end
 
-    it 'can not creates company with name nil' do
-      company_params = { name: nil }
-      Companies::Create.new.call(company_params, account.id)
-      expect(Company.count).to eq(0)
-      expect(Employee.count).to eq(0)
+    context 'with company name nil' do
+      let(:company_params) { { name: nil } }
+      subject(:call) { Companies::Create.new.call(company_params, account.id) }
+
+      it 'not creates company and employee' do
+        expect { call }.not_to change { Employee.count } and change {Company.count}
+      end
     end
 
-    it 'can not creates company with account_id nil' do
-      company_params = { name: 'Any Name' }
-      Companies::Create.new.call(company_params, nil)
-      expect(Company.count).to eq(0)
-      expect(Employee.count).to eq(0)
+    context 'with account_id nil' do
+      let(:company_params) { { name: 'Any Name' } }
+      subject(:call) { Companies::Create.new.call(company_params, nil) }
+
+      it 'not creates company and employee' do
+        expect { call }.not_to change { Employee.count } and change {Company.count}
+      end
     end
   end
 end
