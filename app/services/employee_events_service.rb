6cb2ed173269fe9982326638f80cdfd1f0c76690
Artur <rutraniloms@icloud.com>
2019-08-19 15:21:42 +0300

    add group_by meth, event_params

--- app/services/employee_events_service.rb
+++ app/services/employee_events_service.rb
@@ -3,20 +3,18 @@
 class EmployeeEventsService
   attr_reader :employee, :events_dates
 
-  def initialize(employee, params, day)
-    @employee = employee
-    @events_dates = if params[:start_period].present?
+  def initialize(event_params, day)
+    @employee = Employee.find(event_params[:employee])
+    @events_dates = if event_params[:start_period].present?
                       employee.events
                               .where('start_period >= ? AND end_period <= ?',
-                                     params[:start_period], params[:end_period])
+                                     event_params[:start_period], event_params[:end_period])
                     else
                       employee.events.where('start_period <= ? AND end_period >= ?', day, day)
                     end
   end
 
   def events
-    events_dates.each_with_object({}) do |event, events|
-      events[event.start_period] = [event]
-    end
+    events_dates.group_by(&:start_period)
   end
 end
