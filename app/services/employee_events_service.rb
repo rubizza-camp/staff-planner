96869fb215a32d455acea92cc311c0f710b42654
Artur <rutraniloms@icloud.com>
2019-08-19 16:29:58 +0300

    add scope in employee_event_Service

--- app/services/employee_events_service.rb
+++ app/services/employee_events_service.rb
@@ -4,17 +4,15 @@ class EmployeeEventsService
   attr_reader :employee, :events_dates
 
   def initialize(event_params, day)
-    @employee = employee
+    @employee = Employee.find(event_params[:employee])
     @events_dates = if event_params[:start_period].present?
-                      employee.events
-                              .where('start_period >= ? AND end_period <= ?',
-                                     event_params[:start_period], event_params[:end_period])
+                      Event.employee_events(event_params[:start_period], event_params[:end_period])
                     else
                       employee.events.where('start_period <= ? AND end_period >= ?', day, day)
                     end
   end
 
   def events
-    events_dates.group_by(&:start_date)
+    events_dates.group_by(&:start_period)
   end
 end
