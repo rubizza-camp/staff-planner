0f7981880299df12a83ba263e76b5d493a66f616
Artur <rutraniloms@icloud.com>
2019-08-20 15:43:20 +0300

    fix

--- app/operations/events/create.rb
+++ app/operations/events/create.rb
@@ -2,10 +2,8 @@
 
 module Events
   class Create
-    def call(event_params, company, current_account)
+    def call(event_params, event, current_account)
       if AllowanceService.new(current_account, event_params).allow?
-        event = company.events.build(event_params)
-        event.employee = Employee.find_by(account: current_account, company: company)
         Result::Success.new(event)
       else
         Result::Failure.new(event)
