4babdae2739c1f3ab415570cb0369e4055e64a31
EvgeniyR <richkovevgeniy@mail.ru>
2019-08-21 14:22:07 +0300

    save

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'staffplanner24@gmail.com'
   layout 'mailer'
+
+  def welcome_email
+    mail(to: 'richkovevgeniy@mail.ru', subject: 'test')
+  end
 end
--- app/operations/events/create.rb
+++ app/operations/events/create.rb
@@ -5,6 +5,7 @@ module Events
     def call(event_params, event, current_account)
       if AllowanceService.new(current_account, event_params).allow?
         event.save
+        ApplicationMailer.welcome_email.deliver_now
         Result::Success.new(event)
       else
         Result::Failure.new(event)
