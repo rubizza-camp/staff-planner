9a955c0240728850440e5a0a11051476675fefb9
Artur <rutraniloms@icloud.com>
2019-08-22 19:08:28 +0300

    rubocop config folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  default_url_options :host => "test.host"
-  devise_for :accounts, :controllers => { omniauth_callbacks: 'omniauth_callbacks'}
+  default_url_options host: 'test.host'
+  devise_for :accounts, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   devise_scope :account do
-    root to: "devise/sessions#new"
+    root to: 'devise/sessions#new'
     get '/accounts/sign_out' => 'devise/sessions#destroy'
   end
   resources :accounts, except: %i[new create]
