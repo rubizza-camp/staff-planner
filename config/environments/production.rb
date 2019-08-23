87c5ae6880fbf1403a4019d0acbc9aed32dea542
Artur <rutraniloms@icloud.com>
2019-08-23 14:22:55 +0300

    credentials host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: :localhost }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.heroku[:host] }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
