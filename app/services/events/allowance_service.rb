df42e4886d9c18dc07b2e33426c9bd446ca1ae3f
EvgeniyR <richkovevgeniy@mail.ru>
2019-08-26 14:26:03 +0300

    PR fix 3

---
+++ app/services/events/allowance_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Events
+  class AllowanceService
+    def self.can_create?(rule, event)
+      event_range = (event.start_period.to_date)..(event.end_period.to_date)
+      remaining_days = RemainingDaysService.new.call(event.employee, rule)
+      if event_range.first.strftime('%Y-%m').eql?(event_range.last.strftime('%Y-%m'))
+        remaining_days - event_range.count >= 0
+      else
+        false
+      end
+    end
+  end
+end
