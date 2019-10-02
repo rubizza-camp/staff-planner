da70d3261436ecb9c00d3b0d8fe2b4ae89caf1c5
EvgeniyR <richkovevgeniy@mail.ru>
2019-10-02 11:12:29 +0300

    repair pr

--- app/operations/slack/send_daily_messages.rb
+++ app/operations/slack/send_daily_messages.rb
@@ -3,25 +3,31 @@
 module Slack
   class SendDailyMessages
     def call
-      slack_notifications.each do |slack_notification|
-        text_message = get_text_message(slack_notification.company)
+      companies_events.each do |companie_events|
+        company, events = companie_events
+        text_message = make_text_message(events)
         next if text_message.empty?
 
-        send_message(slack_notification.token, text_message)
+        send_message(company.slack_notification.token, text_message)
       end
     end
 
     private
 
-    def slack_notifications
-      SlackNotification.where(is_enabled: true)
-                       .where.not(token: nil)
-                       .includes(:company)
+    def companies_events
+      date_today = Date.today.to_datetime
+      start_period = date_today.change(hour: Event::END_DAY)
+      end_period = date_today.change(hour: Event::START_DAY)
+      Event.includes(:rule, company: :slack_notification, employee: :account)
+           .where('start_period <= ? and end_period >= ?', start_period, end_period)
+           .where(state: %i[accepted pending])
+           .where(companies: { slack_notifications: { is_enabled: true } })
+           .where.not(companies: { slack_notifications: { token: nil } })
+           .group_by(&:company)
     end
 
-    def get_text_message(company)
+    def make_text_message(events)
       text_message = ''
-      events = get_events_today(company)
       events.each_with_index do |event, i|
         account = event.employee.account
         rule_name = event.rule.name
@@ -30,17 +36,6 @@ module Slack
         text_message += "\n"
       end
       text_message
-    end
-
-    def get_events_today(company)
-      date_today = Date.today.to_datetime
-      start_period = date_today.change(hour: Event::END_DAY)
-      end_period = date_today.change(hour: Event::START_DAY)
-      company
-        .events
-        .where('start_period <= ? and end_period >= ?', start_period, end_period)
-        .where(state: %i[accepted pending])
-        .includes(:rule, employee: :account)
     end
 
     def send_message(token, text_message)
