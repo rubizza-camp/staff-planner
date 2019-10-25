d47cc3d61b7c53f0a962b6e5ce42fe6d01df5f0e
EvgeniyR <richkovevgeniy@mail.ru>
2019-10-25 17:52:57 +0300

    repair pr

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,7 +8,8 @@ class EmployeesController < ApplicationController
   load_and_authorize_resource through: :company
 
   def show
-    @events_presenter = Events::IndexPresenter.new(@company, current_ability, params.merge(employee_id: params[:id]))
+    params.merge!(employee_id: params[:id])
+    @events_presenter = Events::IndexPresenter.new(@company, current_ability, params)
   end
 
   def new
--- app/presenters/events/index_presenter.rb
+++ app/presenters/events/index_presenter.rb
@@ -7,34 +7,30 @@ module Events
       @company = company
       @params = params
       @current_ability = current_ability
-      @from, @to = determine_from_to
+      @from, @to = from_to_from_period(params[:start_period], params[:end_period])
     end
 
+    # rubocop: disable Metrics/AbcSize
     def events
       @events = company.events
                        .accessible_by(current_ability)
                        .range(from, to)
                        .includes(:employee)
+                       .order(updated_at: :desc)
       @events = @events.where(employee_id: params[:employee_id]) if params[:employee_id].present?
       @events
     end
+    # rubocop: enable Metrics/AbcSize
 
     def employees
-      @employees = company.employees
-                          .where.not(account: nil)
-                          .includes(:account)
-                          .accessible_by(current_ability, :show)
+      company.employees
+             .where.not(account: nil)
+             .includes(:account)
+             .accessible_by(current_ability, :show)
+             .order('accounts.surname, accounts.name')
     end
 
     private
-
-    def determine_from_to
-      if params[:start_period].present? || params[:end_period].present?
-        return from_to_from_period(params[:start_period], params[:end_period])
-      end
-
-      from_to_default
-    end
 
     def from_to_from_period(start_period, end_period)
       start_period = - Float::INFINITY unless start_period.present?
@@ -44,10 +40,6 @@ module Events
                      Float::INFINITY
                    end
       [start_period, end_period]
-    end
-
-    def from_to_default
-      [Date.today.beginning_of_day, (Date.today + 1.month).end_of_day]
     end
   end
 end
